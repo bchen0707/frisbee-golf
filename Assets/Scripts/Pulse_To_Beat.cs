aad5818450bd27d705bc30616e55c3a0173b82d1
bchen0707 <71906905+bchen0707@users.noreply.github.com>
2023-09-24 11:50:29 -0400

    Disk Spinning (#2)
    Autoadjust location of disk and spin
    
    Co-authored-by: Shangyi Zhu <62895460+pufferFish-sz@users.noreply.github.com>

---
+++ Assets/Scripts/Pulse_To_Beat.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse_To_Beat : MonoBehaviour
+{
+    [SerializeField] bool _useTestBeat;
+    [SerializeField] float _pulseSize = 1.15f;
+    [SerializeField] float _returnSpeed = 5f;
+    private Vector3 _startSize;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _startSize = transform.localScale;
+        if (_useTestBeat)
+        {
+            StartCoroutine(TestBeat());
+        }
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, _startSize, Time.deltaTime * _returnSpeed);
+    }
+
+    public void Pulse()
+    {
+        transform.localScale = _startSize * _pulseSize;
+    }
+
+    IEnumerator TestBeat()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            Pulse();
+        }
+    }
+}
