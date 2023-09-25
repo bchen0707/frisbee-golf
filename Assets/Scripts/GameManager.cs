9d6624d07dc0f55774ff72a02bbef09eb478fc98
bchen0707 <71906905+bchen0707@users.noreply.github.com>
2023-09-25 17:17:16 -0400

    spin dont work (#9)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance => _instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void SceneTransition()
+}
