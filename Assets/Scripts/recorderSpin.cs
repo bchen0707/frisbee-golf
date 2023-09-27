7091775bf8db29ab99acff4e13b1d56d7d010bcc
Shangyi Zhu <62895460+pufferFish-sz@users.noreply.github.com>
2023-09-27 00:09:32 -0400

    Disc Rotation Fixed (#10)
    Disc when encountered with phonograph collider gradually adjusts to the correct orientation and starts turning after the adjustment.

--- Assets/Scripts/recorderSpin.cs
+++ Assets/Scripts/recorderSpin.cs
@@ -8,24 +8,32 @@ public class recorderSpin : MonoBehaviour
     public float rotateSpeed = 10f;
     public float offset = 1f;
     private bool hasEnteredTriggerZone = false;
-    //private bool hasCollided = false;
     private Transform cubeTransform;
     private Vector3 newLocation;
+    private Quaternion desiredOrientation;
+    private bool hasReachedDesiredOrientation;
 
     [SerializeField] private GameObject grammaphone;
-    
+    private float rotationSmoothing = 2f; // Adjust this value for smoother or faster rotation
+
     // Start is called before the first frame update
     void Start()
     {
         cubeTransform = grammaphone.transform;
-        newLocation = new Vector3(cubeTransform.position.x, offset, cubeTransform.position.y);
+        newLocation = new Vector3(cubeTransform.position.x, cubeTransform.position.y + offset, cubeTransform.position.z);
+        Debug.Log("new location: " + newLocation);
+        // Set the desired Euler angles in degrees.
+        Vector3 desiredRotationEulerAngles = new Vector3(-89.5f, -69.56f, 71.86f);
+
+        // Create a Quaternion from the desired Euler angles.
+        desiredOrientation = Quaternion.Euler(desiredRotationEulerAngles);
+
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("TriggerZone") && hasEnteredTriggerZone == false)
         {
-            Debug.Log("TriggerZoneDetected");
             hasEnteredTriggerZone = true;
         }
     }
@@ -34,13 +42,35 @@ public class recorderSpin : MonoBehaviour
     {
         if(hasEnteredTriggerZone)
         {
-            Debug.Log("yay");
             //update location
             transform.position = newLocation;
+            newLocation = new Vector3(cubeTransform.position.x, cubeTransform.position.y + offset, cubeTransform.position.z);
             //disable gravity
-            GameObject.FindGameObjectWithTag("Grammaphone").GetComponent<Rigidbody>().useGravity = false;
+            GameObject.FindGameObjectWithTag("Disc").GetComponent<Rigidbody>().useGravity = false;
+
             //rotate disk
-            transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f, Space.Self);
+            //transform.Rotate(0f, 0f, rotateSpeed * Time.deltaTime, Space.Self);
+
+            if (!hasReachedDesiredOrientation)
+            {
+                // Rotate disc towards the target rotation smoothly
+                transform.rotation = Quaternion.Slerp(transform.rotation, desiredOrientation, Time.deltaTime * rotationSmoothing);
+
+                if(Quaternion.Angle(transform.rotation, desiredOrientation) < 0.1f)
+                {
+                    hasReachedDesiredOrientation = true;
+                }
+
+            }
+            else
+            {
+                // Calculate the angular velocity to keep rotating around the z-axis
+                float zRotationVelocity = rotateSpeed * Mathf.Deg2Rad;
+
+                // Apply angular velocity to rotate around the local z-axis
+                transform.Rotate(Vector3.forward * zRotationVelocity, Space.Self);
+            }
+
         }
     }
 }
