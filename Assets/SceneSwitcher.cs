8323d60ea2ec2d324be9bd818224a65d7ab13123
Lanning Zhang <46335444+Nllai@users.noreply.github.com>
2023-10-01 12:14:47 -0400

    2D UIUX exploration (#15)
    Start menu scene and Credit scene

---
+++ Assets/SceneSwitcher.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    public void Credit()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player Has Quit The Game");
+    }
+}
