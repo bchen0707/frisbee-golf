c8257384f03784281c43733e77c003b241a3af67
Shangyi Zhu <62895460+pufferFish-sz@users.noreply.github.com>
2023-10-01 16:35:45 -0400

    Grab and Throw
    Grab and Throw allowed in the TestingCandyRoom scene

---
+++ Assets/AnimateHand.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHand : MonoBehaviour
+{
+    public InputActionProperty pinchAnimation;
+    public InputActionProperty gripAnimation;
+    public Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchAnimation.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimation.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", gripValue);
+        // Debug.Log(triggerValue);
+    }
+}
