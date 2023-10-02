85b0d97c53d80d781b9f5a3b72034f0d4ea14d3e
Brandan Chen <bychen@andrew.cmu.edu>
2023-10-02 14:46:36 -0400

    launch disc (hacky throw using trigger)

--- Assets/RecordXRI.cs
+++ Assets/RecordXRI.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class RecordXRI : MonoBehaviour
 {
     public Material[] materials;
+    public Transform launchPosition;
+    public float launchSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void resetMaterials()
     {
         this.GetComponent<MeshRenderer>().materials = materials;
+    }
+
+    public void launchDisc()
+    {
+        GameObject spawnedDisc = Instantiate(this.gameObject);
+        spawnedDisc.transform.position = launchPosition.position;
+        spawnedDisc.GetComponent<Rigidbody>().velocity = launchPosition.forward * launchSpeed;
     }
 }
