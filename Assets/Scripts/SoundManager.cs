52016478901ce877f715b8f738222455a5bf31a3
bchen0707 <bchen0707@gmail.com>
2023-10-04 22:15:37 -0400

    general clean up

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VOManager : MonoBehaviour
+public class SoundManager : MonoBehaviour
 {
+    public AudioSource musicSource;
+
+    public AudioSource VOSource;
     // Start is called before the first frame update
     void Start()
     {
